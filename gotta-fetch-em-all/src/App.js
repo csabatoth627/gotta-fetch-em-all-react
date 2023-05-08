b214eb4d005e0687434893f5c26452c5d95377b2
9Andras <117731271+9Andras@users.noreply.github.com>
2023-05-08 11:33:41 +0200

    impl. first location fetch function in App.js

--- gotta-fetch-em-all/src/App.js
+++ gotta-fetch-em-all/src/App.js
@@ -1,12 +1,31 @@
-
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+
 
 function App() {
-  return (
-    <div className="App">
+  const [data, setData] = useState();
 
-    </div>
-  );
-}
+useEffect (() => {
+  async function fetchEmAll () {
+    try {
+      const respone = await fetch('https://pokeapi.co/api/v2/location/');
+      const pokeData = await respone.json();
+      setData(pokeData);
+      console.log(pokeData);
+    }
+    catch (error) {
+      console.error('Error fetching data:', error)
+    }
+  }
+  fetchEmAll();
+}, []);
+
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
 
 export default App;
