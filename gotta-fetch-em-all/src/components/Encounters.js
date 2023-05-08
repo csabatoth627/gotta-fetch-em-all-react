58f18b508dc9315a9396f0ef1925b743e9ed8ed9
9Andras <117731271+9Andras@users.noreply.github.com>
2023-05-08 20:23:35 +0200

    implement Encounter.js functionality renamed the findsomepokemon component to encounter.js. Right now, we fetch the pokemon we can encounter and its' sprite. Bug: fetches run in rapid succession

---
+++ gotta-fetch-em-all/src/components/Encounters.js
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+
+function Encounters({ locationId }) {
+  const [pokemon, setPokemon] = useState("");
+  const [spriteUrl, setSpriteUrl] = useState("");
+  const [encounterExists, setEncounterExists] = useState(false);
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://pokeapi.co/api/v2/location/${locationId}/`);
+        const data = await response.json();
+        const locationAreas = data.areas;
+        const randomArea = locationAreas[Math.floor(Math.random() * locationAreas.length)];
+        const areaResponse = await fetch(randomArea.url);
+        const areaData = await areaResponse.json();
+
+        if (areaData.pokemon_encounters.length > 0) {
+          const randomEncounter = areaData.pokemon_encounters[Math.floor(Math.random() * areaData.pokemon_encounters.length)];
+          setPokemon(capitalizeFirstLetter(randomEncounter.pokemon.name));
+
+          const pokemonResponse = await fetch(randomEncounter.pokemon.url);
+          const pokemonData = await pokemonResponse.json();
+
+          setSpriteUrl(pokemonData.sprites.front_default);
+          setEncounterExists(true);
+
+        } else {
+          setEncounterExists(false);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [locationId]);
+
+
+  return (
+    <div>
+      {encounterExists ? (
+        <div>
+          <p>A wild <strong>{pokemon}</strong> appeared</p>
+          <img src={spriteUrl} alt={pokemon} />
+        </div>
+      ) : (
+        <p>No pokemon found in this location</p>
+      )}
+    </div>
+  );
+}
+export default Encounters;
