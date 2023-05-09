0249b764c369bcc219762dcc0ee6a77d4ddfa0d7
9Andras <117731271+9Andras@users.noreply.github.com>
2023-05-09 13:01:27 +0200

    add stats to pokemons

--- gotta-fetch-em-all/src/components/Fight.js
+++ gotta-fetch-em-all/src/components/Fight.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 function Fight({ pokemon }) {
   const [usersPokemon, setUsersPokemon] = useState([]);
 
+
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
@@ -10,13 +11,14 @@ function Fight({ pokemon }) {
   useEffect(() => {
     async function fetchPokemons() {
       try {
-        const response1 = await fetch("https://pokeapi.co/api/v2/pokemon/bulbasaur");
+        const response1 = await fetch("https://pokeapi.co/api/v2/pokemon/squirtle");
         const pokemon1 = await response1.json();
         const response2 = await fetch("https://pokeapi.co/api/v2/pokemon/charizard");
         const pokemon2 = await response2.json();
-        const response3 = await fetch("https://pokeapi.co/api/v2/pokemon/poliwhirl");
+        const response3 = await fetch("https://pokeapi.co/api/v2/pokemon/gyarados");
         const pokemon3 = await response3.json();
         setUsersPokemon([pokemon1, pokemon2, pokemon3]);
+        console.log(pokemon1)
       } catch (error) {
         console.error(error);
       }
@@ -27,10 +29,17 @@ function Fight({ pokemon }) {
   return (
     <div>
       <h2>Owned Pokemons</h2>
+      <h3>Who will you choose?</h3>
       {usersPokemon.map((p) => (
         <div key={p.id}>
-          <img src={p.sprites.front_default} alt={p.name} />
-          <p>{capitalizeFirstLetter(p.name)}</p>
+          <button>
+            <img src={p.sprites.front_default} alt={p.name} />
+            <p>{capitalizeFirstLetter(p.name)}</p>
+            <p>HP: {p.stats[0].base_stat}</p>
+            <p>Attack Power: {p.stats[1].base_stat}</p>
+            <p>Defense: {p.stats[2].base_stat}</p>
+          </button>
+
         </div>
       ))}
     </div>
